74856d91258235942153dcff31b5c24381e9fcea
Levi <levi@coassemble.com>
2021-06-09 20:59:52 +1000

    add player stuff

--- entities/player.lua
+++ entities/player.lua
@@ -4,8 +4,9 @@ local positionComponent = require('components.position')
 
 return function()
     local e = entity()
-    e.addComponent(appearanceComponent())
-    e.addComponent(positionComponent())
+    image = love.graphics.newImage('sprites/shine.png')
+    e.addComponent(appearanceComponent(image))
+    e.addComponent(positionComponent(50, 50))
 
     return e
 end
