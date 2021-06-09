73f4a726d6c9067d0b154ed45b6d5f536116032f
Levi <levi@coassemble.com>
2021-06-09 20:59:28 +1000

    fix config

--- conf.lua
+++ conf.lua
@@ -1,60 +1,51 @@
-local config = {
-    identity = nil, -- The name of the save directory (string)
-    appendidentity = false, -- Search files in source directory before save directory (boolean)
-    version = '11.3', -- The LÖVE version this game was made for (string)
-    console = false, -- Attach a console (boolean, Windows only)
-    accelerometerjoystick = true, -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
-    externalstorage = false, -- True to save files (and read from the save directory) in external storage on Android (boolean) 
-    gammacorrect = false, -- Enable gamma-correct rendering, when supported by the system (boolean)
-    audio = {
-        mic = false, -- Request and use microphone capabilities in Android (boolean)
-        mixwithsystem = true -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
-    },
-    window = {
-        title = 'Clarity', -- The window title (string)
-        icon = 'images/tree-icon.png', -- Filepath to an image to use as the window's icon (string)
-        width = 800, -- The window width (number)
-        height = 600, -- The window height (number)
-        borderless = false, -- Remove all border visuals from the window (boolean)
-        resizable = true, -- Let the window be user-resizable (boolean)
-        minwidth = 1, -- Minimum window width if the window is resizable (number)
-        minheight = 1, -- Minimum window height if the window is resizable (number)
-        fullscreen = false, -- Enable fullscreen (boolean)
-        fullscreentype = 'desktop', -- Choose between "desktop" or "exclusive" fullscreen mode (string)
-        vsync = 1, -- Vertical sync mode (number)
-        msaa = 0, -- The number of samples to use with multi-sampled antialiasing (number)
-        depth = nil, -- The number of bits per sample in the depth buffer
-        stencil = nil, -- The number of bits per sample in the stencil buffer
-        display = 1, -- Index of the monitor to show the window in (number)
-        highdpi = false, -- Enable high-dpi mode for the window on a Retina display (boolean)
-        usedpiscale = true, -- Enable automatic DPI scaling when highdpi is true as well (boolean)
-        x = nil, -- The x-coordinate of the window's position in the specified display (number)
-        y = nil, -- The y-coordinate of the window's position in the specified display (number)
-    },
-    modules = {
-        audio = true, -- Enable the audio module (boolean)
-        data = true, -- Enable the data module (boolean)
-        event = true, -- Enable the event module (boolean)
-        font = true, -- Enable the font module (boolean)
-        graphics = true, -- Enable the graphics module (boolean)
-        image = true, -- Enable the image module (boolean)
-        joystick = true, -- Enable the joystick module (boolean)
-        keyboard = true, -- Enable the keyboard module (boolean)
-        math = true, -- Enable the math module (boolean)
-        mouse = true, -- Enable the mouse module (boolean)
-        physics = true, -- Enable the physics module (boolean)
-        sound = true, -- Enable the sound module (boolean)
-        system = true, -- Enable the system module (boolean)
-        thread = true, -- Enable the thread module (boolean)
-        timer = true, -- Enable the timer module (boolean), Disabling it will result 0 dt in update.
-        touch = true, -- Enable the touch module (boolean)
-        video = true, -- Enable the video module (boolean)
-        window = true -- Enable the window module (boolean)
-    }
-}
+love.conf = function(c)
+    c.identity = nil                    -- The name of the save directory (string)
+    c.appendidentity = false            -- Search files in source directory before save directory (boolean)
+    c.version = '11.3'                  -- The LÖVE version this game was made for (string)
+    c.console = false                   -- Attach a console (boolean, Windows only)
+    c.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
+    c.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean)
+    c.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
-love.conf = function()
-    return config
+    c.audio.mic = false                 -- Request and use microphone capabilities in Android (boolean)
+    c.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
+
+    c.window.title = 'Clarity'          -- The window title (string)
+    c.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
+    c.window.width = 800                -- The window width (number)
+    c.window.height = 600               -- The window height (number)
+    c.window.borderless = false         -- Remove all border visuals from the window (boolean)
+    c.window.resizable = true           -- Let the window be user-resizable (boolean)
+    c.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
+    c.window.minheight = 1              -- Minimum window height if the window is resizable (number)
+    c.window.fullscreen = false         -- Enable fullscreen (boolean)
+    c.window.fullscreentype = 'desktop' -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+    c.window.vsync = 1                  -- Vertical sync mode (number)
+    c.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number)
+    c.window.depth = nil                -- The number of bits per sample in the depth buffer
+    c.window.stencil = nil              -- The number of bits per sample in the stencil buffer
+    c.window.display = 1                -- Index of the monitor to show the window in (number)
+    c.window.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean)
+    c.window.usedpiscale = true         -- Enable automatic DPI scaling when highdpi is set to true as well (boolean)
+    c.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
+    c.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
+
+    c.modules.audio = true              -- Enable the audio module (boolean)
+    c.modules.data = true               -- Enable the data module (boolean)
+    c.modules.event = true              -- Enable the event module (boolean)
+    c.modules.font = true               -- Enable the font module (boolean)
+    c.modules.graphics = true           -- Enable the graphics module (boolean)
+    c.modules.image = true              -- Enable the image module (boolean)
+    c.modules.joystick = true           -- Enable the joystick module (boolean)
+    c.modules.keyboard = true           -- Enable the keyboard module (boolean)
+    c.modules.math = true               -- Enable the math module (boolean)
+    c.modules.mouse = true              -- Enable the mouse module (boolean)
+    c.modules.physics = true            -- Enable the physics module (boolean)
+    c.modules.sound = true              -- Enable the sound module (boolean)
+    c.modules.system = true             -- Enable the system module (boolean)
+    c.modules.thread = true             -- Enable the thread module (boolean)
+    c.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
+    c.modules.touch = true              -- Enable the touch module (boolean)
+    c.modules.video = true              -- Enable the video module (boolean)
+    c.modules.window = true             -- Enable the window module (boolean)
 end
-
-return config
