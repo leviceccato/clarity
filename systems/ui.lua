bf926e2c8e0702721ae983679f2cf51fdaab4e48
Levi <levi@coassemble.com>
2021-06-09 23:30:03 +1000

    woopsy

--- systems/ui.lua
+++ systems/ui.lua
@@ -5,7 +5,7 @@ return function()
     s.components = { 'appearance', 'position', 'ui' }
 
     s.draw = function()
-        love.graphics.print("FPS: " .. tostring(love.timer.getFPS( )), 10, 10)
+        love.graphics.print('FPS: ' .. tostring(love.timer.getFPS( )), 10, 10)
         -- local e
         -- for index = 1, #s.entities do
         --     e = s.entities[index]
