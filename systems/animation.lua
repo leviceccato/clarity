177cc4a9e917c728358215d4303c2b81e75f9701
Levi <levi@coassemble.com>
2021-06-13 14:36:35 +1000

    format

--- systems/animation.lua
+++ systems/animation.lua
@@ -4,7 +4,8 @@ return function()
     local s = system({'appearance', 'animation'})
 
     s.update = function(dt)
-        for index = 1, #s.entities do local e = s.entities[index]
+        for index = 1, #s.entities do
+            local e = s.entities[index]
             e.appearance.time = e.appearance.time + dt
             if e.appearance.time >= e.appearance.duration then
                 e.appearance.time = e.appearance.time - e.appearance.duration
