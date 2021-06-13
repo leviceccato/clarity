02044fdd4d14e2fbd2abd927eaa49a9f6401855e
Levi <levi@coassemble.com>
2021-06-13 12:49:43 +1000

    add animation system separate to draw

---
+++ systems/animation.lua
@@ -0,0 +1,16 @@
+local system = require('library.system')
+
+return function()
+    local s = system({'appearance', 'animation'})
+
+    s.update = function(dt)
+        for index = 1, #s.entities do local e = s.entities[index]
+            e.appearance.time = e.appearance.time + dt
+            if e.appearance.time >= e.appearance.duration then
+                e.appearance.time = e.appearance.time - e.appearance.duration
+            end
+        end
+    end
+
+    return s
+end
