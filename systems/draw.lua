68728fd319dbeca0778e60b67c585aa68dbedffd
Levi <levi@coassemble.com>
2021-06-13 12:46:46 +1000

    optimise draw system

--- systems/draw.lua
+++ systems/draw.lua
@@ -1,13 +1,14 @@
 local system = require('library.system')
 
+local lgDraw = love.graphics.draw
+
 return function()
     local s = system({'appearance', 'position'})
 
     s.draw = function()
-        local e
         for index = 1, #s.entities do
-            e = s.entities[index]
-            love.graphics.draw(e.appearance.image, e.position.x, e.position.y)
+            local e = s.entities[index]
+            lgDraw(e.appearance.image, e.position.x, e.position.y)
         end
     end
 
