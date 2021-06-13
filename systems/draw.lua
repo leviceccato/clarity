7b959658915ec75e520b183f4d53d30a63f3e86f
Levi <levi@coassemble.com>
2021-06-13 17:29:31 +1000

    optimise draw and add support for animations

--- systems/draw.lua
+++ systems/draw.lua
@@ -1,6 +1,6 @@
 local system = require('library.system')
 
-local lgDraw = love.graphics.draw
+local graphics = love.graphics
 
 return function()
     local s = system({'appearance', 'position'})
@@ -8,7 +8,20 @@ return function()
     s.draw = function()
         for index = 1, #s.entities do
             local e = s.entities[index]
-            lgDraw(e.appearance.image, e.position.x, e.position.y)
+            if e.animation then
+                graphics.draw(
+                    e.appearance.image,
+                    e.animation.getQuad,
+                    e.position.x,
+                    e.position.y
+                )
+            else
+                graphics.draw(
+                    e.appearance.image,
+                    e.position.x,
+                    e.position.y
+                )
+            end
         end
     end
 
