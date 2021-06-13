72d064ca0bacc0aee08dc02b1c6fcfb4865db3e6
Levi <levi@coassemble.com>
2021-06-13 15:17:19 +1000

    we are using lua not js

--- components/collision.lua
+++ components/collision.lua
@@ -2,7 +2,7 @@ local component = require('library.component')
 
 return function(min, max)
     local c = component('collision')
-    c.min = min || {x = 0, y = 0}
-    c.max = max || {x = 0, y = 0}
+    c.min = min or {x = 0, y = 0}
+    c.max = max or {x = 0, y = 0}
     return c
 end
