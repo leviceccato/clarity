b7be6a1f446ccdb704302c9c723514937ad73d22
Levi Ceccato <levi.ceccato@gmail.com>
2021-06-14 21:27:21 +1000

    make player speed dependant on dt

--- systems/player.lua
+++ systems/player.lua
@@ -3,21 +3,22 @@ local system = require('library.system')
 return function(state)
     local s = system({'position', 'controls'})
 
-    s.update = function()
+    s.update = function(dt)
+        local distance = 200 * dt
         local e
         for index = 1, #s.entities do
             e = s.entities[index]
             if state.controls.up then
-                e.position.y = e.position.y - 2
+                e.position.y = e.position.y - distance
             end
             if state.controls.left then
-                e.position.x = e.position.x - 2
+                e.position.x = e.position.x - distance
             end
             if state.controls.down then
-                e.position.y = e.position.y + 2
+                e.position.y = e.position.y + distance
             end
             if state.controls.right then
-                e.position.x = e.position.x + 2
+                e.position.x = e.position.x + distance
             end
         end
     end
