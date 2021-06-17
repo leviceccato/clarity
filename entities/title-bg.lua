3b2e35aa01e610d7a55087153dbed148730e9d5f
Levi <levi@coassemble.com>
2021-06-17 23:31:33 +1000

    add title bg entity

---
+++ entities/title-bg.lua
@@ -0,0 +1,13 @@
+local entity = require('library.entity')
+local appearance = require('components.appearance')
+local position = require('components.position')
+
+return function()
+    local e = entity()
+    local image = love.graphics.newImage('assets/sprites/title-bg.png')
+
+    e.addComponent(appearance(image))
+    e.addComponent(position(0, 0))
+
+    return e
+end
