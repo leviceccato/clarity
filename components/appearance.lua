1f59b76999782b78c07cf734c9dd758568943525
Levi <levi@coassemble.com>
2021-06-18 23:15:22 +1000

    allow appearance components to just have width and height

--- components/appearance.lua
+++ components/appearance.lua
@@ -1,14 +1,18 @@
 local component = require('library.component')
 
-return function(image)
-    local width = image:getWidth()
-    local height = image:getHeight()
+return function(image, width, height)
     local c = component('appearance')
-    c.image = image
-    c.frame = love.graphics.newQuad(
-        0, 0,
-        width, height,
-        width, height
-    )
+    if image then
+        width = image:getWidth()
+        height = image:getHeight()
+        c.image = image
+        c.frame = love.graphics.newQuad(
+            0, 0,
+            width, height,
+            width, height
+        )
+    end
+    c.width = width
+    c.height = height
     return c
 end
