422e462be7ab0306aa4e55a374d3750aed49bee1
Levi <levi@coassemble.com>
2021-06-19 11:20:49 +1000

    add hover system

--- systems/hover.lua
+++ systems/hover.lua
@@ -10,11 +10,18 @@ return function(state)
         local e
         for index = 1, #s.entities do
             e = s.entities[index]
-            local isWithinX = mouseX >= e.position.x and mouseX <= e.position.x + e.appearance.width
-            local isWithinY = mouseY >= e.position.y and mouseY <= e.position.y + e.appearance.height
+            local width = e.appearance.width
+            local height = e.appearance.height
+            if e.animation then
+                local _, _, w, h = e.animation.getFrame():getViewport()
+                width = w
+                height = h
+            end
+            local isWithinX = mouseX >= e.position.x and mouseX <= e.position.x + width
+            local isWithinY = mouseY >= e.position.y and mouseY <= e.position.y + height
             local isHovered = isWithinX and isWithinY
             if isHovered ~= e.hover.isHovered then
-                print('hover')
+                print('hover ' .. tostring(isHovered))
             end
             e.hover.isHovered = isHovered
         end
