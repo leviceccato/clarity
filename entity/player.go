e002581ed847ee7ad67e656292fa8abef49ffed5
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-14 20:06:36 +1000

    Load actual player and animation

--- entity/player.go
+++ entity/player.go
@@ -2,27 +2,18 @@ package entity
 
 import (
 	"fmt"
-	_ "image/png"
 
 	"github.com/leviceccato/clarity/component"
-
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-
-var image *ebiten.Image
 
 func NewPlayerEntity() (*Entity, error) {
 	e := NewEntity()
 	e.Position = &component.Position{X: 10, Y: 10}
 	e.Size = &component.Size{Width: 10, Height: 10}
-	if image == nil {
-		var err error
-		image, _, err = ebitenutil.NewImageFromFile("assets/test3.png")
-		if err != nil {
-			return e, fmt.Errorf("loading player image: %s", err)
-		}
+	appearance, err := component.NewAppearance("assets/player.png", "assets/player.json")
+	if err != nil {
+		return e, fmt.Errorf("creating appearance component: %s", err)
 	}
-	e.Appearance = &component.Appearance{Image: image}
+	e.Appearance = appearance
 	return e, nil
 }
