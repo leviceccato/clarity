cae109f51439b6709741b3769ec40292da4b0258
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-15 12:33:50 +1000

    Draw player in middle of screen

--- world/start.go
+++ world/start.go
@@ -2,7 +2,6 @@ package world
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/leviceccato/clarity/entity"
 	"github.com/leviceccato/clarity/system"
@@ -21,19 +20,13 @@ func NewStartWorld(state gameState) (*start, error) {
 	w.AddSystem(system.NewDrawSystem())
 	w.AddSystem(system.NewAnimationSystem())
 
-	var randX float64
-	var randY float64
-	for i := 0; i < 1; i++ {
-		randX = rand.Float64() * float64(state.GetWindowWidth())
-		randY = rand.Float64() * float64(state.GetWindowHeight())
-		player, err := entity.NewPlayerEntity()
-		if err != nil {
-			return nil, fmt.Errorf("creating player entity: %s", err)
-		}
-		player.Position.X = randX
-		player.Position.Y = randY
-		w.AddEntity(player)
+	player, err := entity.NewPlayerEntity()
+	if err != nil {
+		return nil, fmt.Errorf("creating player entity: %s", err)
 	}
+	player.Position.X = float64(state.GetWindowWidth()) / 2
+	player.Position.Y = float64(state.GetWindowHeight()) / 2
+	w.AddEntity(player)
 
 	w.updateSystems()
 
