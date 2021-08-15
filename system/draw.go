34a77fc0009fccfd0fa5ff2756cf2c2a685e4d52
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-15 12:34:12 +1000

    Have draw system use the current frame

--- system/draw.go
+++ system/draw.go
@@ -2,7 +2,6 @@ package system
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -24,18 +23,17 @@ func NewDrawSystem() *draw {
 
 func (s *draw) Load() {}
 
-func (s *draw) Update() {
-	for _, e := range s.entities {
-		e.Position.X += (rand.Float64() * 5) - 2.5
-		e.Position.Y += (rand.Float64() * 5) - 2.5
-	}
-}
+func (s *draw) Update() {}
 
 func (s *draw) Draw(screen *ebiten.Image) {
+	var options *ebiten.DrawImageOptions
 	for _, e := range s.entities {
-		options := &ebiten.DrawImageOptions{}
+		options = &ebiten.DrawImageOptions{}
 		options.GeoM.Translate(e.Position.X, e.Position.Y)
-		screen.DrawImage(e.Appearance.Image, options)
+		screen.DrawImage(
+			e.Appearance.Image.SubImage(*e.Appearance.Frames[e.Appearance.Frame]).(*ebiten.Image),
+			options,
+		)
 	}
 	ebitenutil.DebugPrint(screen, fmt.Sprintf(
 		"TPS: %0.2f\nFPS: %0.2f",
