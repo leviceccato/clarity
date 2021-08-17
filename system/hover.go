7fa0da66add3168ff3580eb12395812002b8a743
Levi Ceccato <levi.ceccato@gmail.com>
2021-08-17 21:19:45 +1000

    Add hover system logic

--- system/hover.go
+++ system/hover.go
@@ -20,7 +20,34 @@ func NewHoverSystem() *hoverSystem {
 
 func (s *hoverSystem) Load() {}
 
-func (s *hoverSystem) Update() {}
+func (s *hoverSystem) Update() {
+	mouseX, mouseY := ebiten.CursorPosition()
+	var hasHoverChanged, isHovered, hasHoverSequence bool
+	for _, e := range s.entities {
+		// Check if mouse is within x and y ranges
+		isHovered = mouseX >= int(e.Position.X) && mouseX <= int(e.Position.X)+int(e.Size.Width) &&
+			mouseY >= int(e.Position.Y) && mouseY <= int(e.Position.Y)+int(e.Size.Height)
+		hasHoverChanged = isHovered != e.Hover.IsHovered
+		if hasHoverChanged {
+			e.Hover.IsHovered = isHovered
+			// For animations allow toggling a 'hover' sequence
+			if e.Appearance == nil {
+				continue
+			}
+			_, hasHoverSequence = e.Appearance.Sequences["hover"]
+			if !hasHoverSequence {
+				continue
+			}
+			if isHovered {
+				e.Appearance.PreviousSequence = e.Appearance.Sequence
+				e.Appearance.Sequence = "hover"
+			} else {
+				e.Appearance.PreviousSequence = "hover"
+				e.Appearance.Sequence = e.Appearance.PreviousSequence
+			}
+		}
+	}
+}
 
 func (s *hoverSystem) Draw(screen *ebiten.Image) {}
 
