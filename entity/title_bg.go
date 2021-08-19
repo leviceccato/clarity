14be78d4d0e5d542571faad842f0de1bce843c2e
Levi <levi@coassemble.com>
2021-08-19 10:35:44 +1000

    Add title_bg entity

---
+++ entity/title_bg.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"fmt"
+
+	"github.com/leviceccato/clarity/component"
+)
+
+func NewTitleBgEntity() (*Entity, error) {
+	e := NewEntity()
+	e.Position = &component.PositionComponent{X: 0, Y: 0}
+	e.Size = &component.SizeComponent{}
+	appearance, err := component.NewAppearanceComponent("assets/title_bg.png", "")
+	if err != nil {
+		return e, fmt.Errorf("creating appearance component: %s", err)
+	}
+	e.Appearance = appearance
+	return e, nil
+}
