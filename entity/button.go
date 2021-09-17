bccfb67eec40f01347bb5966de8b5725e39aaf5a
Levi Ceccato <levi.ceccato@gmail.com>
2021-09-17 20:44:08 +1000

    Add color field

--- entity/button.go
+++ entity/button.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"fmt"
+	"image/color"
 
 	"github.com/leviceccato/clarity/component"
 )
@@ -9,13 +10,14 @@ import (
 type ButtonEntityOptions struct {
 	X, Y, Width, Height    float64
 	Text, Image, Animation string
+	Color                  color.NRGBA
 }
 
 func NewButtonEntity(options *ButtonEntityOptions) (*Entity, error) {
 	e := NewEntity()
 	e.Position = &component.PositionComponent{X: options.X, Y: options.Y}
 	e.Size = &component.SizeComponent{Width: options.Width, Height: options.Height}
-	e.Text = component.NewTextComponent(options.Text)
+	e.Text = &component.TextComponent{Content: options.Text, Color: options.Color}
 	appearance, err := component.NewAppearanceComponent(options.Image, options.Animation)
 	if err != nil {
 		return e, fmt.Errorf("creating appearance component: %s", err)
