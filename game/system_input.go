45ba2581eb1df095353631ea7d7616270e18a50a
Levi <levi@coassemble.com>
2022-04-14 22:51:46 +1000

    Update game state on user input

--- game/system_input.go
+++ game/system_input.go
@@ -2,33 +2,56 @@ package game
 
 import (
 	"github.com/leviceccato/clarity/engine"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 func newInputSystem(g *Game) *engine.System {
 	s := engine.NewSystem("input", []string{})
 
 	s.Update = func() error {
-		// for _, entityId := range s.EntityIds {
-		// 	e := g.GetEntity(entityId)
-		// 	// for mouseInput, control := range s.MouseInputs {
-		// 	// 	if inpututil.IsMouseButtonJustPressed(mouseInput) {
-		// 	// 		x, y := ebiten.CursorPosition()
-		// 	// 		s.SetControl(control, &component.InputData{X: x, Y: y})
-		// 	// 	}
-		// 	// 	if inpututil.IsMouseButtonJustReleased(mouseInput) {
-		// 	// 		s.SetControl(control, nil)
-		// 	// 	}
-		// 	// }
+		for input, cmd := range g.mouseInputs {
+			x, y := engine.CursorPosition()
 
-		// 	// for keyInput, control := range s.KeyInputs {
-		// 	// 	if inpututil.IsKeyJustPressed(keyInput) {
-		// 	// 		s.SetControl(control, &component.InputData{})
-		// 	// 	}
-		// 	// 	if inpututil.IsKeyJustReleased(keyInput) {
-		// 	// 		s.SetControl(control, nil)
-		// 	// 	}
-		// 	// }
-		// }
+			if inpututil.IsMouseButtonJustPressed(input) {
+				g.setInput(cmd, &inputData{
+					isStart: true,
+					x:       x,
+					y:       y,
+				})
+				continue
+			}
+
+			if inpututil.IsMouseButtonJustReleased(input) {
+				g.setInput(cmd, &inputData{
+					isStart: false,
+					x:       x,
+					y:       y,
+				})
+				continue
+			}
+
+			if !ebiten.IsMouseButtonPressed(input) {
+				g.setInput(cmd, nil)
+			}
+		}
+
+		for input, cmd := range g.keyInputs {
+			if inpututil.IsKeyJustPressed(input) {
+				g.setInput(cmd, &inputData{isStart: true})
+				continue
+			}
+
+			if inpututil.IsKeyJustReleased(input) {
+				g.setInput(cmd, &inputData{isStart: false})
+				continue
+			}
+
+			if !ebiten.IsKeyPressed(input) {
+				g.setInput(cmd, nil)
+			}
+		}
 
 		return nil
 	}
