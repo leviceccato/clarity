67a4da1053fab2fb73a23bc4e292a92fe5eb91b1
Levi <levi@coassemble.com>
2022-04-14 15:46:07 +1000

    Split game init and game def into separate files

--- game/0_game.go
+++ game/0_game.go
@@ -1,111 +1,34 @@
 package game
 
 import (
-	"fmt"
-	"image"
 	"image/color"
 
 	"github.com/leviceccato/clarity/asset"
 	"github.com/leviceccato/clarity/engine"
 	"github.com/leviceccato/clarity/logger"
 
-	"github.com/hajimehoshi/ebiten/v2"
 	"golang.org/x/image/font"
-	"golang.org/x/text/language"
 )
 
 type Game struct {
 	*engine.Game
-	state
 
 	// Globals
 	fonts      map[string]font.Face
 	colors     map[string]color.NRGBA
 	translator *asset.Translator
 	logger     *logger.Logger
+
+	// Settings
+	InputBindings map[string]string
+
+	// State
+	isCursorHovering bool
+	// Controls         map[component.Control]*component.InputData
+	// MouseInputs      map[ebiten.MouseButton]component.Control
+	// KeyInputs        map[ebiten.Key]component.Control
 }
 
-type Options struct {
-	RenderWidth, RenderHeight int
-
-	Title string
-
-	Logger *logger.Logger
-}
-
-func CreateAndRun(options *Options) error {
-	g := &Game{Game: engine.NewGame()}
-
-	// Init translations
-	translator := asset.NewTranslator()
-	err := translator.AddLocalizer("translation/en.json", language.English)
-	if err != nil {
-		return fmt.Errorf("adding localizer: %w", err)
-	}
-	translator.Lang = language.English
-	g.translator = translator
-
-	// Load fonts
-	lanaPixel, err := asset.LoadFont(
-		"font/lana_pixel.ttf",
-		11,
-		72,
-		font.HintingNone,
-	)
-	if err != nil {
-		return fmt.Errorf("loading font: %w", err)
-	}
-	g.fonts = map[string]font.Face{
-		"lana_pixel": lanaPixel,
-	}
-
-	// Add icons
-	icon16, err := asset.LoadIcon("icon.iconset/icon_16x16.png")
-	if err != nil {
-		return fmt.Errorf("loading icon_16: %w", err)
-	}
-	icon32, err := asset.LoadIcon("icon.iconset/icon_32x32.png")
-	if err != nil {
-		return fmt.Errorf("loading icon_32: %w", err)
-	}
-	ebiten.SetWindowIcon([]image.Image{icon32, icon16})
-
-	g.RenderWidth = options.RenderWidth
-	g.RenderHeight = options.RenderHeight
-	ebiten.SetWindowSize(g.RenderWidth*2, g.RenderHeight*2)
-	ebiten.SetWindowResizable(true)
-	ebiten.SetWindowTitle(options.Title)
-
-	// Set colours
-	g.colors = map[string]color.NRGBA{
-		"fg-title": {255, 240, 157, 255},
-	}
-
-	// Init systems
-	g.AddSystems(
-		newAnimationSystem(g),
-		newCursorSystem(g),
-		newDrawSystem(g),
-		newEventSystem(g),
-		newHoverSystem(g),
-		newInputSystem(g),
-		newPlayableSystem(g),
-	)
-
-	// Init worlds
-	g.AddWorlds(
-		newTitleWorld(g),
-		newStartWorld(g),
-	)
-
-	// Set title as active world
-	g.ActivateWorlds("title")
-
-	// Run the game
-	err = g.Run()
-	if err != nil {
-		return fmt.Errorf("running game: %w", err)
-	}
-
-	return nil
+func (g *Game) setIsCursorHovering(to bool) {
+	g.isCursorHovering = to
 }
