4fc88bd915c0c33cd0ca61e2ece780b924572d01
Levi <levi@coassemble.com>
2022-04-18 15:29:30 +1000

    Add missing component

--- game/entity_cursor.go
+++ game/entity_cursor.go
@@ -9,6 +9,8 @@ import (
 func newCursorEntity(g *Game) (*engine.Entity, error) {
 	e := g.NewEntity()
 
+	e.AddComponent(&cursorComponent{})
+
 	e.AddComponent(&positionComponent{
 		X: 10,
 		Y: 10,
